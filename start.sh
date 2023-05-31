806ce6d963d834f7c53da5d5edc56519c95d16ab
rohanhackeryt <119674298+rohanhackeryt@users.noreply.github.com>
2023-05-31 21:07:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/rohanbhai27/Rnp-Shortner-Converter-Bot.git /Rnp-Shortner-Converter-Bot
-cd /Rnp-Shortner-Converter-Bot
+git clone https://github.com/rohanbhai27/Rnp-Shortner-Converter-Bot.git /Shortner-Converter-Bot
+cd /Shortner-Converter-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
